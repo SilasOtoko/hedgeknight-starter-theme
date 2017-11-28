f7b07fee20cbb28fd52cb21edea8fa3024eef984
Josiah Schaefer <josiahschaefer@SGCs-MacBook-Pro.local>
2017-11-28 11:55:26 -0500

    apparently forgot to include desktop menu

--- wp-content/themes/sleeping_giant/header.php
+++ wp-content/themes/sleeping_giant/header.php
@@ -73,84 +73,98 @@
 
 		    </div>
 
-				<div class="mobile-menu clearfix">
+				<nav role="navigation" class="desktop-menu">
 
-					<button tabindex="0" onclick="openNav()" class="toggle-sidebar">
-						<div class="hamburger-label">Menu</div>
-						<div class="hamburger" style="max-width: 40px;">
+		      <?php
+					 	$defaults = array(
+						 	'container' => false,
+						 	'div' => false,
+						 	'theme_location' => 'main-menu'
+					 	);
+					 wp_nav_menu( $defaults );
+				 	?>
 
-							<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-								 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-								<g>
+				</nav>
+
+					<div class="mobile-menu clearfix">
+
+						<button tabindex="0" onclick="openNav()" class="toggle-sidebar">
+							<div class="hamburger-label">Menu</div>
+							<div class="hamburger" style="max-width: 40px;">
+
+								<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+									 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
 									<g>
-										<path class="st0" d="M491.3,235.3H20.7C9.3,235.3,0,244.6,0,256s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
-											C512,244.6,502.7,235.3,491.3,235.3z"/>
+										<g>
+											<path class="st0" d="M491.3,235.3H20.7C9.3,235.3,0,244.6,0,256s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
+												C512,244.6,502.7,235.3,491.3,235.3z"/>
+										</g>
 									</g>
-								</g>
-								<g>
 									<g>
-										<path class="st0" d="M491.3,78.4H20.7C9.3,78.4,0,87.7,0,99.1s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
-											S502.7,78.4,491.3,78.4z"/>
+										<g>
+											<path class="st0" d="M491.3,78.4H20.7C9.3,78.4,0,87.7,0,99.1s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
+												S502.7,78.4,491.3,78.4z"/>
+										</g>
 									</g>
-								</g>
-								<g>
 									<g>
-										<path class="st0" d="M491.3,392.2H20.7C9.3,392.2,0,401.5,0,412.9s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
-											S502.7,392.2,491.3,392.2z"/>
+										<g>
+											<path class="st0" d="M491.3,392.2H20.7C9.3,392.2,0,401.5,0,412.9s9.3,20.7,20.7,20.7h470.6c11.4,0,20.7-9.3,20.7-20.7
+												S502.7,392.2,491.3,392.2z"/>
+										</g>
 									</g>
-								</g>
-							</svg>
+								</svg>
 
-						</div>
-					</button>
+							</div>
+						</button>
 
-					<div id="mySidenav" class="sidenav">
-						<div class="sidenav-wrapper">
+						<div id="mySidenav" class="sidenav">
+							<div class="sidenav-wrapper">
 
-							<div id="sidebar">
-								<div class="mobile-nav-header clearfix">
-									<div class="close-sidenav clearfix">
-										<button tabindex="0" class="closebtn" onclick="closeNav()">
+								<div id="sidebar">
+									<div class="mobile-nav-header clearfix">
+										<div class="close-sidenav clearfix">
+											<button tabindex="0" class="closebtn" onclick="closeNav()">
 
-											<img src="<?php bloginfo( 'template_directory' ); ?>/img/close.svg" />
+												<img src="<?php bloginfo( 'template_directory' ); ?>/img/close.svg" />
 
-										</button>
+											</button>
+
+										</div>
+									</div>
+
+									<div class="mobile-navigation">
+
+										<nav class="mobile-menu">
+
+											<?php
+											 	$defaults = array(
+												 	'container' => false,
+												 	'div' => false,
+												 	'theme_location' => 'hamburger-menu'
+											 	);
+											 wp_nav_menu( $defaults );
+										 	?>
+
+										</nav>
 
 									</div>
 								</div>
-
-								<div class="mobile-navigation">
-
-									<nav class="mobile-menu">
-
-										<?php
-										 	$defaults = array(
-											 	'container' => false,
-											 	'div' => false,
-											 	'theme_location' => 'hamburger-menu'
-										 	);
-										 wp_nav_menu( $defaults );
-									 	?>
-
-									</nav>
-
-								</div>
 							</div>
 						</div>
+
+						<script>
+							/* Set the width of the side navigation to 250px */
+							function openNav() {
+								jQuery('.sidenav').toggleClass('sidenav-open');
+							}
+
+							/* Set the width of the side navigation to 0 */
+							function closeNav() {
+								jQuery('.sidenav').toggleClass('sidenav-open');
+							}
+						</script>
+
 					</div>
-
-					<script>
-						/* Set the width of the side navigation to 250px */
-						function openNav() {
-							jQuery('.sidenav').toggleClass('sidenav-open');
-						}
-
-						/* Set the width of the side navigation to 0 */
-						function closeNav() {
-							jQuery('.sidenav').toggleClass('sidenav-open');
-						}
-					</script>
-
 				</div>
 
 			</div>
